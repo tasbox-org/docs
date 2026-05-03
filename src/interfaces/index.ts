2a607c2ddf3ccf44b182fbafdd54ea8467828c66
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-03 16:56:26 +0100

    Group type exports

---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export type * from "./class";
+export type * from "./constant";
+export type * from "./enum";
+export type * from "./function";
+export type * from "./library";
+export type * from "./property";
+export type * from "./realm";
+export type * from "./type";
